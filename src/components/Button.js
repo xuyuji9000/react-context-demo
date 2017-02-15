b89d8e8ad51120c3efa9291ef7198ad1ea3ccde7
Karl Xu <karl.yogi.xu@gmail.com>
2017-02-15 13:15:14 +0800

    use context

--- src/components/Button.js
+++ src/components/Button.js
@@ -4,11 +4,15 @@ class Button extends Component
 {
     render() {
         return (
-            <button style={{background: this.props.color}}>
+            <button style={{background: this.context.color}}>
                 {this.props.children}
             </button>
         );
     }
 }
+
+Button.contextTypes = {
+    color: React.PropTypes.string
+};
 
 export default Button;
--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -3,16 +3,27 @@ import Message from "./Message";
 
 class MessageList extends Component
 {
+    getChildContext() 
+    {
+        return {
+            color: "purple"
+        };
+    }
+
     render()
     {
         const color = "purple";
-        const children = this.props.messages.map(message => {
-            return <Message text={message.text} color={color}></Message>;
+        const children = this.props.messages.map((message, index) => {
+            return <Message text={message.text} key={index}></Message>;
         });
         return(
             <div>{children}</div>
         );
     }
 }
+
+MessageList.childContextTypes = {
+    color: React.PropTypes.string
+};
 
 export default MessageList;
